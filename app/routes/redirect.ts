27644e7d8d2fdda9124f5010f2156bbffdc5b444
Jacob Ebey <jacob.ebey@live.com>
2023-10-05 23:06:50 -0700

    chore: close up redirect route

--- app/routes/redirect.ts
+++ app/routes/redirect.ts
@@ -3,8 +3,13 @@ import { redirect, type LoaderFunctionArgs } from "@remix-run/node";
 export function loader({ request }: LoaderFunctionArgs) {
   const url = new URL(request.url);
   const location = url.searchParams.get("location");
-  if (!location) {
-    throw new Error("No location provided");
+  if (
+    !location ||
+    (!location.startsWith("https://htmx.org/") &&
+      !location.startsWith("https://hyperscript.org/") &&
+      !location.startsWith("https://remix.run/"))
+  ) {
+    throw new Error("Invalid location provided");
   }
 
   return redirect(location);
